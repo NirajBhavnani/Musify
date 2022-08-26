0d3d783f6935b95f4fed9aa1b68a6ae0e0296b82
nbhavna <niraj.bhavnani@morningstar.com>
2022-08-27 03:00:29 +0530

    Navbar component added

---
+++ src/components/Navbar.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="navbar">
+    <nav>
+      <img src="@/assets/music-logo.png" alt="Logo" />
+      <h1><router-link :to="{ name: 'home' }">Musify!</router-link></h1>
+      <div class="links">
+        <button>Log out</button>
+        <router-link class="btn" :to="{ name: 'signup' }">Sign up</router-link>
+        <router-link class="btn" :to="{ name: 'login' }">Log in</router-link>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.navbar {
+  padding: 16px 10px;
+  margin-bottom: 60px;
+  background: white;
+}
+nav {
+  display: flex;
+  align-items: center;
+  max-width: 1200px;
+  margin: 0 auto;
+}
+nav img {
+  max-height: 60px;
+}
+nav h1 {
+  margin-left: 20px;
+}
+nav .links {
+  margin-left: auto;
+}
+nav .links a,
+button {
+  margin-left: 16px;
+  font-size: 14px;
+}
+</style>
