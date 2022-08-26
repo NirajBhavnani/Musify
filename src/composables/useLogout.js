2af9b54c33d6ea5b04f741c440eb013dd15b8f7b
nbhavna <niraj.bhavnani@morningstar.com>
2022-08-27 02:29:43 +0530

    Adding a pending state in the composables

--- src/composables/useLogout.js
+++ src/composables/useLogout.js
@@ -3,21 +3,25 @@ import { auth } from "../firebase/config";
 
 // refs
 const error = ref(null);
+const isPending = ref(false);
 
 // logout function
 const logout = async () => {
   error.value = null;
+  isPending.value = true;
 
   try {
     await auth.signOut();
+    isPending.value = false;
   } catch (err) {
     console.log(err.message);
+    isPending.value = false;
     error.value = err.message;
   }
 };
 
 const useLogout = () => {
-  return { error, logout };
+  return { error, logout, isPending };
 };
 
 export default useLogout;
--- src/composables/useSignup.js
+++ src/composables/useSignup.js
@@ -2,9 +2,11 @@ import { ref } from "vue";
 import { auth } from "../firebase/config";
 
 const error = ref(null);
+const isPending = ref(false);
 
 const signup = async (email, password, displayName) => {
   error.value = null;
+  isPending.value = true;
 
   try {
     const res = await auth.createUserWithEmailAndPassword(email, password);
@@ -13,16 +15,18 @@ const signup = async (email, password, displayName) => {
     }
     await res.user.updateProfile({ displayName });
     error.value = null;
+    isPending.value = false;
 
     return res;
   } catch (err) {
     console.log(err.message);
+    isPending.value = false;
     error.value = err.message;
   }
 };
 
 const useSignup = () => {
-  return { error, signup };
+  return { error, signup, isPending };
 };
 
 export default useSignup;
