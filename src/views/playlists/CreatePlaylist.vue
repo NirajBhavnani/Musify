6b5381830f06e8db6bc7112f2808195c4d49cffb
nbhavna <niraj.bhavnani@morningstar.com>
2022-08-27 19:14:10 +0530

    Added form for creating playlist

---
+++ src/views/playlists/CreatePlaylist.vue
@@ -0,0 +1,40 @@
+<template>
+  <form @submit.prevent="handleCreate">
+    <h4>Create New Playlist</h4>
+    <input type="text" required placeholder="Playlist title" v-model="title" />
+    <textarea
+      required
+      placeholder="Playlist description..."
+      v-model="description"
+    ></textarea>
+    <label>Upload playlist cover image</label>
+    <input type="file" />
+    <div class="error"></div>
+    <button>Create</button>
+  </form>
+</template>
+
+<script>
+import { ref } from "@vue/reactivity";
+export default {
+  setup() {
+    const title = ref("");
+    const description = ref("");
+
+    const handleCreate = () => {};
+
+    return { title, description, handleCreate };
+  },
+};
+</script>
+
+<style>
+label {
+  font-size: 12px;
+  display: block;
+  margin-top: 30px;
+}
+button {
+  margin-top: 20px;
+}
+</style>
