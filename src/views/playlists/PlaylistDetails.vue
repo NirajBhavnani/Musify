39fa1d0219c19a43246c69da16c942f160f6fb45
nbhavna <niraj.bhavnani@morningstar.com>
2022-08-28 22:07:52 +0530

    PlaylistDetails improved and single doc fetched from firebase

--- src/views/playlists/PlaylistDetails.vue
+++ src/views/playlists/PlaylistDetails.vue
@@ -1,12 +1,73 @@
 <template>
-  <h2>Playlist Details. ID - {{ pid }}</h2>
+  <div v-if="error" class="error">{{ error }}</div>
+  <div v-if="playlist" class="playlist-details">
+    <div class="playlist-info">
+      <div class="cover">
+        <img :src="playlist.coverUrl" alt="cover" />
+      </div>
+      <h2>{{ playlist.title }}</h2>
+      <p class="username">Created by {{ playlist.userName }}</p>
+      <p class="description">{{ playlist.description }}</p>
+    </div>
+
+    <div class="song-list">
+      <p>Song list</p>
+    </div>
+  </div>
 </template>
 
 <script>
+import getDocument from "@/composables/getDocument";
+
 export default {
   props: ["pid"],
+  // in order to use pid inside setup, accept props as a parameter
+  setup(props) {
+    // we are referring document as playlist
+    const { error, document: playlist } = getDocument("playlists", props.pid);
+
+    return { error, playlist };
+  },
 };
 </script>
 
-<style>
+<style scoped>
+.playlist-details {
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  gap: 80px;
+}
+.cover {
+  overflow: hidden;
+  border-radius: 20px;
+  position: relative;
+  padding: 160px;
+}
+.cover img {
+  display: block;
+  position: absolute;
+  top: 0;
+  left: 0;
+  min-width: 100%;
+  min-height: 100%;
+  max-width: 200%;
+  max-height: 200%;
+}
+.playlist-info {
+  text-align: center;
+}
+.playlist-info h2 {
+  text-transform: capitalize;
+  font-size: 28px;
+  margin-top: 20px;
+}
+.playlist-info p {
+  margin-bottom: 20px;
+}
+.username {
+  color: #999;
+}
+.description {
+  text-align: left;
+}
 </style>
